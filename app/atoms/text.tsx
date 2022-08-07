59d4e43f313033ee877f15805c35665d3528553d
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-07 11:47:15 +0430

    Added the basic state manager

---
+++ app/atoms/text.tsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const textD = atom({
+  key: "textD",
+  default: "hello",
+});
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,13 @@
+import { useRecoilState } from "recoil";
+import { textD } from "~/atoms/text";
+
 export default function Index() {
+  const [textx, setTextX] = useRecoilState(textD);
   return (
     <div>
       <h1 className="text-secondary-500">Title 1</h1>
-      <h2>Title 2</h2>
-      <h3>Title 3</h3>
+      <h2>{textx}</h2>
+      <input value={textx} onChange={(e) => setTextX(e.currentTarget.value)} />
       <h4>Title 4</h4>
       <h5>Title 5</h5>
       <h6>Title 6</h6>
