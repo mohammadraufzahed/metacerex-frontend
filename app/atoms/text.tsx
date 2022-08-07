8aa6c3dc4327b7cff30c9855e001886fd4d02e40
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-07 16:51:40 +0430

    Removed the junk function

--- app/atoms/text.tsx
+++ app/atoms/text.tsx
@@ -1,11 +1,6 @@
-import { atom, useRecoilState } from "recoil";
+import { atom } from "recoil";
 
 export const count = atom({
   key: "count-useless",
   default: 0,
 });
-
-export function useSetCount(counts: Number) {
-  const [_, setCounts] = useRecoilState(count);
-  setCounts(counts);
-}
