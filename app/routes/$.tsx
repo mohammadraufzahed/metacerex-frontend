6a3d772afcc767934325206280dba8bbad7c52c2
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-11 19:10:49 +0430

    Added the 404 page

---
+++ app/routes/$.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardLayout from "~/layouts/DashboardLayout";
+
+const $ = () => {
+  return (
+    <DashboardLayout>
+      <span className="font-vazir w-full h-full flex items-center justify-center">
+        صفحه پیدا نشد
+      </span>
+    </DashboardLayout>
+  );
+};
+
+export default $;
