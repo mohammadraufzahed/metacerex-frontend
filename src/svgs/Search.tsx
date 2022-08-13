cc89db12b29998f55d1af0f3d4e1877164b1eaaa
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-13 23:07:40 +0430

    Added the new SVG component

---
+++ src/svgs/Search.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+
+const Search = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={24}
+    height={24}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M11 20a9 9 0 1 0 0-18 9 9 0 0 0 0 18ZM18.93 20.69c.53 1.6 1.74 1.76 2.67.36.85-1.28.29-2.33-1.25-2.33-1.14-.01-1.78.88-1.42 1.97Z"
+      stroke="#292D32"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={props.className}
+    />
+  </svg>
+);
+
+export default Search;
