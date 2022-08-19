ef9de59c7f018e7f83990ce858aae7b7cbb20172
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-19 18:01:17 +0430

    Fix: Fixed the height size

--- src/components/CryptoList/ListSelector.tsx
+++ src/components/CryptoList/ListSelector.tsx
@@ -5,7 +5,7 @@ const ListContainerDesktop = lazy(() => import("./ListContainer.desktop"));
 
 const ListSelector: React.FC = () => {
   return (
-    <div className="relative lg:max-h-[510px] lg:min-h-[500px] lg:overflow-y-hidden">
+    <div className="relative lg:max-h-[440px] lg:min-h-[440px] lg:overflow-y-hidden">
       <ListContainerMobile />
       <ListContainerDesktop />
     </div>
