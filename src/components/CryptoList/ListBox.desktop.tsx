bf927afeafc5d3dd58dfdbacc4727691bb4c7be0
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-19 18:01:37 +0430

    Fix: Fixed the height size

--- src/components/CryptoList/ListBox.desktop.tsx
+++ src/components/CryptoList/ListBox.desktop.tsx
@@ -4,7 +4,7 @@ const ListItem = lazy(() => import("./ListItem.desktop"));
 
 const ListBox: React.FC = () => {
   return (
-    <div className="flex-auto max-h-[44vh] overflow-y-scroll scrollbar-vertical">
+    <div className="flex-auto max-h-[400px] overflow-y-scroll scrollbar-vertical">
       <table className="table-auto w-full border-spacing-y-5 border-separate">
         <thead className="w-full border-b-neutral-900 border-b-[1px]">
           <tr className='w-full font-vazir relative font-bold text-base after:content-[""] after:w-full after:h-[1px] after:bg-neutral-900 after:absolute after:left-0 after:-bottom-2'>
