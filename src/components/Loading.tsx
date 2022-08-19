53ac1dffb13cb3ed203bc51a7f3fc462ebccc4b7
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-19 18:23:38 +0430

    Fix: Changed the Loading Screen Background

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Loading = () => {
   return (
-    <div className="w-full flex-auto flex flex-col gap-3 items-center justify-center min-h-[inherit] bg-neutral-200">
+    <div className="w-full flex-auto flex flex-col gap-3 items-center justify-center min-h-[inherit] bg-background-50">
       <motion.img
         src="/svgs/logo-white.svg"
         width={50}
