7c30ee6334026a98564d711961b9feb4373ea8c7
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-25 14:32:47 +0430

    Improve: Renamed the hook file

---
+++ src/hooks/useNews.ts
@@ -0,0 +1,9 @@
+import { httpClient } from "../axios";
+import { NewsArticleList } from "../types/API";
+
+export async function useNews(): Promise<NewsArticleList[]> {
+  const news: NewsArticleList[] = await httpClient
+    .get("contents/news", {})
+    .then((data) => data.data);
+  return news;
+}
