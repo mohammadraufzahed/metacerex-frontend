025642a93f4b0b50bca4add5494e564580b0eaff
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-30 17:54:59 +0430

    Update: Changed the quote asset

--- src/hooks/useTickers.ts
+++ src/hooks/useTickers.ts
@@ -4,7 +4,7 @@ import type { TickerTable } from "../types/API";
 export async function useTickers(): Promise<TickerTable[]> {
   const request = await httpClient.get("cryptobase/tickers", {
     params: {
-      quote_asset: "toman",
+      quote_asset: "usdt",
     },
   });
   return request.data.results;
