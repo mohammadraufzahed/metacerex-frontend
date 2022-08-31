3fb3e1b9ec6b5256e0fac736091d65fd98bdd2d9
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-31 14:35:25 +0430

    Improvment: Improved the MenuItem responsiveness

--- src/components/AuthenticationPage/MenuItem.tsx
+++ src/components/AuthenticationPage/MenuItem.tsx
@@ -22,7 +22,7 @@ const MenuItem: React.FC<PropsT> = ({ text, active, onClick, only }) => {
   };
   return (
     <motion.span
-      className={`w-full text-center font-vazir font-bold text-xl py-3 border-b-2 cursor-pointer ${
+      className={`w-full text-center font-vazir font-bold text-base sm:text-xl py-3 border-b-2 cursor-pointer ${
         only ? (only == "desktop" ? "hidden lg:block" : "lg:hidden") : ""
       }`}
       variants={spanAnimation}
