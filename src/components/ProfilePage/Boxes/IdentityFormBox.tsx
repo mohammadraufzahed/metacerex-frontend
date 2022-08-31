9d428812282d9ca4d98468386b58a91bbf7ff010
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-08-31 13:54:29 +0430

    Fix & New: Fixed the paths and added the new IdentityFormBox component

---
+++ src/components/ProfilePage/Boxes/IdentityFormBox.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProfileFormLayout from "../../../layouts/ProfileFormLayout";
+
+const IdentityFormBox = () => {
+  return (
+    <ProfileFormLayout title="اطلاعات هویتی">IdentityFormBox</ProfileFormLayout>
+  );
+};
+
+export default IdentityFormBox;
--- src/components/ProfilePage/forms/PersonalInformationForm.tsx
+++ src/components/ProfilePage/forms/PersonalInformationForm.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import InformationAuthStatusBox from "../InformationAuthStatusBox";
+import IdentityFormBox from "../Boxes/IdentityFormBox";
+import InformationAuthStatusBox from "../Boxes/InformationAuthStatusBox";
 
 const PersonalInformationForm = () => {
   return (
-    <div className="w-full py-2 lg:py-8">
+    <div className="w-full py-2 lg:py-8 flex flex-col gap-6 lg:gap-8">
       <InformationAuthStatusBox />
+      <IdentityFormBox />
     </div>
   );
 };
