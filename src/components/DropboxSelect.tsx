d286a9d69640440402e89e53d83cf0bbb672ce61
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-09-07 17:24:51 +0430

    Improvment: Added the new animation and search box
    Added a new animation to the list childs and also added a search box for
    better ux

--- src/components/DropboxSelect.tsx
+++ src/components/DropboxSelect.tsx
@@ -1,21 +1,36 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { nanoid } from "nanoid";
 import React, { useState } from "react";
+import Search from "../svgs/Search";
+import Input from "./Input";
 
 type PropsT = {
   placeholder: string;
+  hasMemo?: boolean;
   onChange: (name: string) => void;
-  list: { text: string; value: string }[];
+  list: {
+    text: string | undefined;
+    value: string;
+    icon?: string;
+    memo?: string;
+  }[];
 };
 
-const DropboxSelect: React.FC<PropsT> = ({ placeholder, list, onChange }) => {
+const DropboxSelect: React.FC<PropsT> = ({
+  placeholder,
+  hasMemo,
+  list,
+  onChange,
+}) => {
   // States
-  const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(true);
+  const [search, setSearch] = useState<string>("");
   return (
     <motion.div
       initial="close"
       variants={{
         open: {
-          height: 350,
+          height: 400,
         },
         close: {
           height: 41,
@@ -30,6 +45,9 @@ const DropboxSelect: React.FC<PropsT> = ({ placeholder, list, onChange }) => {
         onClick={() => setOpen((open) => !open)}
       >
         <span className="font-vazir font-normal text-base">{placeholder}</span>
+        {hasMemo ? (
+          <span className="font-vazir font-normal text-base">ممو دارد</span>
+        ) : null}
         <motion.img
           variants={{
             open: {
@@ -45,19 +63,46 @@ const DropboxSelect: React.FC<PropsT> = ({ placeholder, list, onChange }) => {
           src="/svgs/arrow-down.svg"
         />
       </div>
+      <div className="relative mb-2 w-full">
+        <Input
+          isPrimary
+          id={`search_${nanoid()}`}
+          name={`search_${nanoid()}`}
+          label=""
+          value={search}
+          onChange={({ currentTarget }) => setSearch(currentTarget.value)}
+        />
+        <Search className="absolute top-4 left-2" />
+      </div>
       <div className="flex-auto bg-neutral-50 flex flex-col overflow-y-scroll scrollbar-vertical">
-        {list.map((item, key) => (
-          <span
-            className="w-full text-center font-vazir text-primary-700 px-2 py-2 border-b-[1px] border-neutral-300 font-bold"
-            key={key}
-            onClick={() => {
-              onChange(item.text);
-              setOpen(false);
-            }}
-          >
-            {item.text}
-          </span>
-        ))}
+        <AnimatePresence mode="sync">
+          {list
+            .filter(
+              (item) =>
+                item.text?.toLowerCase().includes(search.toLocaleLowerCase()) ||
+                item.value.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((item, key) => (
+              <motion.div
+                initial={{ x: 430, background: "rgb(250 250 250)" }}
+                whileHover={{ background: "rgb(245, 245, 245)" }}
+                whileTap={{ background: "rgb(229, 229, 229)" }}
+                className="w-full cursor-pointer font-vazir text-primary-700 px-2 py-2 border-b-[1px] font-bold flex flex-row items-center gap-2"
+                animate={{ x: 0 }}
+                exit={{ x: 430 }}
+                transition={{ duration: 0.4, type: "tween" }}
+                key={key}
+                onTap={() => {
+                  onChange(item.value);
+                  setOpen(false);
+                }}
+              >
+                {item.icon ? <img src={item.icon} width={24} /> : null}
+                <span>{item.text}</span>
+                {item.memo ? <span className="mr-auto">ممو دارد</span> : null}
+              </motion.div>
+            ))}
+        </AnimatePresence>
       </div>
     </motion.div>
   );
