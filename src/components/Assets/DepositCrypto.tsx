c193888578b23c0f5d80c7ae917b60d0cd5a8f6a
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-09-07 17:30:26 +0430

    Improvment: Added the functionality to deposit forms

--- src/components/Assets/DepositCrypto.tsx
+++ src/components/Assets/DepositCrypto.tsx
@@ -1,10 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useFormik } from "formik";
 import { motion } from "framer-motion";
-import React, { useState } from "react";
+import { getItem } from "localforage";
+import React, { useEffect, useState } from "react";
+import { getDepositAssets } from "../../functions/assets";
+import useCustomToast from "../../hooks/useCustomToast";
 import AnimatedCopy from "../../svgs/AnimatedCopy";
+import { AssetList, Networks } from "../../types/API";
 import Button from "../AuthenticationPage/Button";
 import DropboxSelect from "../DropboxSelect";
 import Input from "../Input";
 import RulesButton from "./RulesButton";
+import * as yup from "yup";
+import { httpClient } from "../../axios";
+import { useNavigate } from "react-router-dom";
 
 type PropsT = {
   onRuleClick: () => void;
@@ -12,7 +21,68 @@ type PropsT = {
 
 const DepositCrypto: React.FC<PropsT> = ({ onRuleClick }) => {
   // States
-  const [currentCurrency, setCurrentCurrency] = useState<string>("");
+  const [currentCurrencyCode, setCurrentCurrencyCode] = useState<string>("");
+  const [currentCurrency, setCurrentCurrency] = useState<AssetList | null>(
+    null
+  );
+  const [selectedNetwork, setSelectedNetwork] = useState<Networks | null>();
+  const [copied, setCopied] = useState<0 | 1>(0);
+  const navigate = useNavigate();
+  const DepositCryptoFormik = useFormik({
+    initialValues: {
+      asset: "",
+      network: "",
+      txid_hash: "",
+    },
+    validationSchema: yup.object({
+      asset: yup.string().required("لطفا ارز مورد نظر را انتخاب کنید"),
+      network: yup.string().required("لطفا شبکه مورد نظر را انتخاب کنید"),
+      txid_hash: yup.string().required("لطفا کد تراکنش را وارد نمایید"),
+    }),
+    async onSubmit(params) {
+      return await httpClient
+        .post("spot/deposit/", {
+          ...params,
+        })
+        .then((res) => {
+          if (res.status == 201) {
+            navigate("/dashboard/profile", { replace: true });
+            useCustomToast(
+              "bottom-right",
+              "success",
+              "درخواست واریز با موفقیت ثبت شد"
+            );
+          }
+        });
+    },
+  });
+  // Queries
+  const assetsQuery = useQuery(["assets_list"], getDepositAssets);
+  // Effects
+  useEffect(() => {
+    if (assetsQuery.data) {
+      setCurrentCurrency(
+        assetsQuery.data.filter((item) => item.code == currentCurrencyCode)[0]
+      );
+    }
+  }, [currentCurrencyCode]);
+  useEffect(() => {
+    if (selectedNetwork) {
+      DepositCryptoFormik.setFieldValue("network", selectedNetwork.name);
+    }
+  }, [selectedNetwork]);
+  useEffect(() => {
+    if (currentCurrency) {
+      DepositCryptoFormik.setFieldValue("asset", currentCurrency.code);
+    }
+  }, [currentCurrency]);
+  useEffect(() => {
+    for (const [key, value] of Object.entries(DepositCryptoFormik.errors)) {
+      if (value) {
+        useCustomToast("bottom-right", "error", value);
+      }
+    }
+  }, [DepositCryptoFormik.errors]);
   return (
     <motion.div
       key="crypto"
@@ -22,22 +92,69 @@ const DepositCrypto: React.FC<PropsT> = ({ onRuleClick }) => {
       className="w-full flex flex-col items-center gap-8"
     >
       <DropboxSelect
-        placeholder={currentCurrency == "" ? "رمز ارز" : currentCurrency}
-        onChange={(name) => setCurrentCurrency(name)}
-        list={[...Array(100).fill({ text: "بیت کوین", value: "btc" })]}
+        placeholder={
+          currentCurrency
+            ? currentCurrency.name_farsi
+              ? currentCurrency.name_farsi
+              : currentCurrency.name ?? ""
+            : "رمز ارز"
+        }
+        onChange={(name) => {
+          setCurrentCurrencyCode(name);
+          setSelectedNetwork(null);
+        }}
+        list={
+          assetsQuery.data
+            ? assetsQuery.data
+                .filter((item) => item.code != "TOMAN")
+                .map((item) => ({
+                  text: item.name_farsi == "" ? item.name : item.name_farsi,
+                  value: item.code,
+                  icon: item.icon,
+                }))
+            : []
+        }
       />
+      {currentCurrency ? (
+        <DropboxSelect
+          list={currentCurrency.networks.map((item) => ({
+            text: item.name,
+            value: item.name,
+          }))}
+          onChange={(name) =>
+            setSelectedNetwork(
+              currentCurrency.networks.filter((item) => item.name == name)[0]
+            )
+          }
+          placeholder={
+            selectedNetwork
+              ? selectedNetwork.name
+              : "لطفا شبکه مورد نظر را انتخاب کنید."
+          }
+        />
+      ) : null}
       <div className="w-full flex flex-row items-center justify-between font-vazir font-normal text-sm">
         <div className="w-max flex flex-row items-center gap-2.5">
           <img src="/svgs/wallet.svg" width={20} />
           <span>
-            {currentCurrency !== ""
+            {currentCurrency
               ? `موجودی کیف پول شما`
               : "لطفا ارز مورد نظر را انتخاب کنید."}
           </span>
         </div>
         <div className="flex flex-row items-center gap-2">
-          <span>{currentCurrency != "" ? 31231.31231 : "-"}</span>
-          <span>{currentCurrency}</span>
+          {currentCurrency ? (
+            <>
+              <span>{currentCurrency.balance}</span>
+              <span>
+                {currentCurrency.name_farsi
+                  ? currentCurrency.name_farsi
+                  : currentCurrency.name}
+              </span>
+            </>
+          ) : (
+            <span>-</span>
+          )}
         </div>
       </div>
       <div className="flex flex-col w-full items-center justify-between">
@@ -49,20 +166,44 @@ const DepositCrypto: React.FC<PropsT> = ({ onRuleClick }) => {
           آدرس ولت در شبکه انتقال مورد نظر خود را کپی کنید
         </span>
         <div className="w-full mt-6 border-[1px] rounded-lg border-primary-700">
-          <span className="w-full border-b-2 border-primary-700 flex flex-row items-center justify-center font-vazir font-normal text-base">
-            -
+          <span className="w-full border-b-2 border-primary-700 flex flex-row items-center justify-center font-vazir font-normal text-base px-2 py-3 text-primary-700">
+            {selectedNetwork ? selectedNetwork.site_wallet_address : "-"}
           </span>
           <div className="w-full py-3.5 px-3 flex flex-row justify-between items-center">
-            {currentCurrency == "" ? (
+            {currentCurrency ? (
+              <span></span>
+            ) : (
               <span className="font-vazir justify-self-start font-normal text-xs">
                 لطفا رمز ارز مورد نظر خود را انتخاب نمایید.
               </span>
-            ) : (
-              <span></span>
             )}
             <AnimatedCopy
               className="stroke-neutral-400 cursor-pointer"
-              copied={0}
+              copied={copied}
+              onClick={() => {
+                if (selectedNetwork) {
+                  navigator.clipboard.writeText(
+                    selectedNetwork.site_wallet_address
+                  );
+                  setCopied(1);
+                  setTimeout(() => setCopied(0), 2500);
+                  useCustomToast(
+                    "bottom-right",
+                    "success",
+                    "آدرس ولت با موقیت کپی شد"
+                  );
+                } else {
+                  useCustomToast(
+                    "bottom-right",
+                    "error",
+                    currentCurrency
+                      ? "هیچ ارزی انتخواب نشده است"
+                      : selectedNetwork
+                      ? "هیچ شبکه ای انتخاب نشده است"
+                      : ""
+                  );
+                }
+              }}
             />
           </div>
         </div>
@@ -77,7 +218,17 @@ const DepositCrypto: React.FC<PropsT> = ({ onRuleClick }) => {
         <span className="font-vazir font-light text-xs mt-2 mb-4">
           پس از انتقال، کد تراکنش (hash) را کپی و در باکس زیر وارد کنید
         </span>
-        <Input isPrimary fullWidth />
+        <Input
+          label=""
+          type="text"
+          name="txid_hash"
+          id="txid_hash"
+          value={DepositCryptoFormik.values.txid_hash}
+          error={DepositCryptoFormik.errors.txid_hash}
+          onChange={DepositCryptoFormik.handleChange}
+          isPrimary
+          fullWidth
+        />
         <span className="font-vazir font-light text-xs mt-4 text-neutral-700">
           کاربر گرامی، ثبت کد تراکنش (txid یا hash) برای واریز رمز ارز اجباری
           است. لطفا دقت فرمایید که کد تراکنش صحیح را تا حداکثر 2 ساعت پس از
@@ -87,7 +238,13 @@ const DepositCrypto: React.FC<PropsT> = ({ onRuleClick }) => {
         </span>
       </div>
       <RulesButton onTap={onRuleClick} />
-      <Button text="تایید واریز" fullWidth className="bg-primary-500" />
+      <Button
+        onClick={DepositCryptoFormik.submitForm}
+        loading={DepositCryptoFormik.isSubmitting}
+        text="تایید واریز"
+        fullWidth
+        className="bg-primary-500"
+      />
     </motion.div>
   );
 };
--- src/components/Assets/DepositToman.tsx
+++ src/components/Assets/DepositToman.tsx
@@ -12,6 +12,10 @@ import RulesButton from "./RulesButton";
 import Button from "../AuthenticationPage/Button";
 import * as yup from "yup";
 import CardSelectBox from "./CardSelectBox";
+import { useRecoilValue } from "recoil";
+import { statusData } from "../../atoms/status";
+import { httpClient } from "../../axios";
+import useCustomToast from "../../hooks/useCustomToast";
 
 type PropsT = {
   onRuleClick: (event: MouseEvent) => void;
@@ -20,6 +24,7 @@ type PropsT = {
 const DepositToman: React.FC<PropsT> = ({ onRuleClick }) => {
   // States
   const navigate = useNavigate();
+  const statusD = useRecoilValue(statusData);
   const tomanDepositFormik = useFormik({
     initialValues: {
       amount: 0,
@@ -30,8 +35,34 @@ const DepositToman: React.FC<PropsT> = ({ onRuleClick }) => {
         .number()
         .required("مقدار واریزی وارد نشده است.")
         .min(100000, "حداقل واریزی ۱۰۰ هزار تومان میباشد"),
+      selected_card: yup
+        .string()
+        .required("لطفا کارت مورد نظر را انتخاب کنید.")
+        .length(16, "لطفا کارت مورد نظر را انتخاب کنید."),
     }),
-    async onSubmit() {},
+    async onSubmit(form) {
+      return await httpClient
+        .post("shetab/deposit/", {
+          amount_toman: form.amount,
+          gateway: statusD?.shetab_gateways[0][0],
+          short_desc: "",
+          card_number: form.selected_card,
+          callback_url: window.location.origin,
+        })
+        .then((res) => {
+          if (res.status === 201) {
+            useCustomToast(
+              "bottom-right",
+              "success",
+              "درخواست شما ثبت شد. به درگاه منتقل میشوید ..."
+            );
+            setTimeout(
+              () => (window.location.href = res.data.redirect_url),
+              2500
+            );
+          }
+        });
+    },
   });
   // Queries
   const cardsQuery = useQuery(["cards_deposit"], getCards);
@@ -42,13 +73,14 @@ const DepositToman: React.FC<PropsT> = ({ onRuleClick }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.2 }}
-      className="w-full flex flex-col gap-4"
+      className="w-full flex flex-col gap-4 justify-center items-center"
     >
       <div className="w-full flex flex-col gap-2">
         <span className="font-vazir font-normal text-sm">
           مبلغ مورد نظر جهت واریز
         </span>
         <Input
+          label=""
           name="amount"
           id="amount"
           type="number"
@@ -62,10 +94,13 @@ const DepositToman: React.FC<PropsT> = ({ onRuleClick }) => {
       <CardSelectBox
         card_value={tomanDepositFormik.values.selected_card}
         fieldUpdater={tomanDepositFormik.setFieldValue}
+        error={tomanDepositFormik.errors.selected_card}
       />
       <RulesButton onTap={onRuleClick} />
       <Button
         text="تایید و واریز"
+        onClick={() => tomanDepositFormik.submitForm()}
+        loading={tomanDepositFormik.isSubmitting}
         className="bg-primary-500 md:mt-60"
         fullWidth
       />
