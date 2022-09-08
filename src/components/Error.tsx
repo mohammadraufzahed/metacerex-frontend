d5517d9eb5ff67fe855bf7a996174379765a7f0f
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-09-08 11:31:04 +0430

    Improvment: Improved the error ui

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,23 +1,18 @@
 import React from "react";
-import { AiFillWarning } from "react-icons/ai";
 import { motion } from "framer-motion";
 
 const Error: React.FC = () => {
   return (
     <motion.div
-      className="bg-neutral-50 flex items-center rounded-lg border-error border-[1px] justify-center flex-auto"
+      className="bg-transparent w-full flex flex-col gap-4 font-vazir font-bold text-base text-error items-center rounded-sm border-error border-[1px] justify-center flex-auto"
       initial={{ borderColor: "rgba(0,0,0,0)" }}
       animate={{ borderColor: "#EE6055" }}
       transition={{ repeat: Infinity, repeatType: "reverse", duration: 1 }}
     >
-      <span className="font-vazir text-center font-extrabold text-error">
-        <div>
-          <span className="text-2xl flex flex-row items-center justify-center gap-2">
-            <AiFillWarning /> خطا
-          </span>
-          <p className="text-xl">این قسمت به مشکل برخورد</p>
-        </div>
+      <span className="w-max">
+        <img src="/svgs/emoji-sad.svg" width={100} />
       </span>
+      <p className="text-xl">این قسمت به مشکل برخورد</p>
     </motion.div>
   );
 };
