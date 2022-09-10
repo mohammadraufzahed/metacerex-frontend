9880d54876d36ba240c2145e870231f9deecc3dd
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-09-10 17:15:51 +0430

    Fix: Fixed the resolver

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "node:path";
 
-const prodAlias = {
-  "@tanstack/react-query-devtools": path.resolve(
-    __dirname,
-    "node_modules",
-    "@tanstack",
-    "react-query-devtools",
-    "build",
-    "cjs",
-    "noop.js"
-  ),
-};
+const prodAlias = {};
 const devAlias = {};
 
 // https://vitejs.dev/config/
